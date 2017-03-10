0a80e3659cd57723d2cb5c55a437483bdbc63ed6
ttdat89 <ttdat89@gmail.com>
2017-03-10 14:08:05 +0700

    change CoreImage to GPUImage

--- src/ios/ImageFilter.h
+++ src/ios/ImageFilter.h
@@ -9,9 +9,13 @@
 #import <Foundation/Foundation.h>
 #import <Cordova/CDV.h>
 
+#import <MyGPUFramework/GPUImage.h>
+
 @interface ImageFilter : CDVPlugin
 
 - (void)applyEffect:(CDVInvokedUrlCommand*)command;
 - (void)applyEffectForReview:(CDVInvokedUrlCommand*)command;
+- (void)applyEffectForThumbnail:(CDVInvokedUrlCommand *)command;
 
 @end
+
--- src/ios/ImageFilter.m
+++ src/ios/ImageFilter.m
@@ -1,6 +1,5 @@
 //
 //  ImageFilter.m
-//  HelloWorld
 //
 //  Created by dat tran on 3/3/17.
 //
@@ -13,23 +12,15 @@
 static CIContext *context;
 static UIImage *currentEditingImage;
 static UIImage *currentPreviewImage;
+static UIImage *currentThumbnailImage;
 static NSString *currentImagePath = nil;
 static CGSize screenSize;
-
-//CIPhotoEffectChrome
-//CIPhotoEffectFade
-//CIPhotoEffectInstant
-//CIPhotoEffectMono
-//CIPhotoEffectNoir
-//CIPhotoEffectProcess
-//CIPhotoEffectTonal
-//CIPhotoEffectTransfer
 
 static NSString* toBase64(NSData* data) {
     SEL s1 = NSSelectorFromString(@"cdv_base64EncodedString");
     SEL s2 = NSSelectorFromString(@"base64EncodedString");
     SEL s3 = NSSelectorFromString(@"base64EncodedStringWithOptions:");
-    
+
     if ([data respondsToSelector:s1]) {
         NSString* (*func)(id, SEL) = (void *)[data methodForSelector:s1];
         return func(data, s1);
@@ -46,10 +37,10 @@ static NSString* toBase64(NSData* data) {
 
 - (void)pluginInitialize {
     [super pluginInitialize];
-    
+
     EAGLContext *eaglCxt = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
     context = [CIContext contextWithEAGLContext:eaglCxt];
-    
+
     screenSize = [[UIScreen mainScreen] bounds].size;
 }
 
@@ -58,27 +49,33 @@ static NSString* toBase64(NSData* data) {
     NSURL *pathUrl = [NSURL URLWithString:path];
     path = pathUrl.path;
     NSString *filterType = [command argumentAtIndex:1 withDefault:nil];
-    if (path.length && filterType.length) {
-        if (![currentImagePath isEqualToString:path]) {
-            currentImagePath = path;
-            currentEditingImage = [UIImage imageWithContentsOfFile:path];
-            
-            float ratio = screenSize.width / currentEditingImage.size.width;
-            CGSize newSize = CGSizeMake(currentEditingImage.size.width * ratio, currentEditingImage.size.height * ratio);
-            UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
-            [currentEditingImage drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
-            currentPreviewImage = UIGraphicsGetImageFromCurrentImageContext();
-            UIGraphicsEndImageContext();
-        }
+    NSNumber *thumbnailRatio = [command argumentAtIndex:3 withDefault:@(0.1f)];
+
+    if (path.length && filterType.length && ![currentImagePath isEqualToString:path]) {
+        currentImagePath = path;
+        currentEditingImage = [UIImage imageWithContentsOfFile:path];
+
+        float ratio = screenSize.width / currentEditingImage.size.width;
+        CGSize newSize = CGSizeMake(currentEditingImage.size.width * ratio, currentEditingImage.size.height * ratio);
+        UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
+        [currentEditingImage drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+        currentPreviewImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+
+        CGSize thumbnailSize = CGSizeMake(currentPreviewImage.size.width * [thumbnailRatio floatValue], currentPreviewImage.size.height * [thumbnailRatio floatValue]);
+        UIGraphicsBeginImageContextWithOptions(thumbnailSize, NO, 0.0);
+        [currentPreviewImage drawInRect:CGRectMake(0, 0, thumbnailSize.width, thumbnailSize.height)];
+        currentThumbnailImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
     }
 }
 
 - (void)applyEffect:(CDVInvokedUrlCommand*)command {
     [self validateInput:command];
-    
+
     NSString *filterType = [command argumentAtIndex:1 withDefault:nil];
     NSNumber *compressionQuality = [command argumentAtIndex:2 withDefault:@(0.5)];
-    
+
     [self filterImage:currentEditingImage filter:filterType compressionQuality:compressionQuality completion:^(NSData *data) {
         CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:toBase64(data)];
         dispatch_sync(dispatch_get_main_queue(), ^{
@@ -89,11 +86,25 @@ static NSString* toBase64(NSData* data) {
 
 - (void)applyEffectForReview:(CDVInvokedUrlCommand*)command {
     [self validateInput:command];
-    
+
     NSString *filterType = [command argumentAtIndex:1 withDefault:nil];
     NSNumber *compressionQuality = [command argumentAtIndex:2 withDefault:@(0.5)];
-    
+
     [self filterImage:currentPreviewImage filter:filterType compressionQuality:compressionQuality completion:^(NSData *data) {
+        CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:toBase64(data)];
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        });
+    }];
+}
+
+- (void)applyEffectForThumbnail:(CDVInvokedUrlCommand *)command {
+    [self validateInput:command];
+
+    NSString *filterType = [command argumentAtIndex:1 withDefault:nil];
+    NSNumber *compressionQuality = [command argumentAtIndex:2 withDefault:@(0.5)];
+
+    [self filterImage:currentThumbnailImage filter:filterType compressionQuality:compressionQuality completion:^(NSData *data) {
         CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:toBase64(data)];
         dispatch_sync(dispatch_get_main_queue(), ^{
             [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
@@ -104,20 +115,14 @@ static NSString* toBase64(NSData* data) {
 - (void)filterImage:(UIImage *)image filter:(NSString *)filterType compressionQuality:(NSNumber *)quality completion:(void(^)(NSData *))completion {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         @autoreleasepool {
-            CIImage *ciImage = [[CIImage alloc] initWithImage:image];
-            
-            ciImage = [self applySelectedEffect:ciImage effect:filterType];
-            
-            CGImageRef ref = [context createCGImage:ciImage fromRect:ciImage.extent];
-            UIImage *img = [[UIImage alloc] initWithCGImage:ref];
-            NSData *data = UIImageJPEGRepresentation(img, [quality floatValue]);
+            UIImage *result = [self applySelectedEffect:image effect:filterType];
+            NSData *data = UIImageJPEGRepresentation(result, [quality floatValue]);
             completion(data);
-            CFRelease(ref);
         }
     });
 }
 
-- (CIImage *)applySelectedEffect:(CIImage *)image effect:(NSString *)effect {
+- (UIImage *)applySelectedEffect:(UIImage *)image effect:(NSString *)effect {
     if ([effect isEqualToString:@"aged"])
         return [self applyAgedEffect:image];
     else if ([effect isEqualToString:@"blackWhite"])
@@ -136,7 +141,7 @@ static NSString* toBase64(NSData* data) {
         return nil;
 }
 
-- (CIImage *)applyAgedEffect:(CIImage *)img {
+- (UIImage *)applyAgedEffect:(UIImage *)img {
     return [self applyFilter:img
                   saturation:@(0.4072f)
                   brightness:@(-0.1495f)
@@ -150,7 +155,7 @@ static NSString* toBase64(NSData* data) {
                         blue:@(0.5232f)];
 }
 
-- (CIImage *)applyBlackWhiteEffect:(CIImage *)img {
+- (UIImage *)applyBlackWhiteEffect:(UIImage *)img {
     return [self applyFilter:img
                   saturation:@(0.0f)
                   brightness:nil
@@ -164,7 +169,7 @@ static NSString* toBase64(NSData* data) {
                         blue:nil];
 }
 
-- (CIImage *)applyColdEffect:(CIImage *)img {
+- (UIImage *)applyColdEffect:(UIImage *)img {
     return [self applyFilter:img
                   saturation:@(0.3093f)
                   brightness:@(-0.0722f)
@@ -178,7 +183,7 @@ static NSString* toBase64(NSData* data) {
                         blue:nil];
 }
 
-- (CIImage *)applyRosyEffect:(CIImage *)img {
+- (UIImage *)applyRosyEffect:(UIImage *)img {
     return [self applyFilter:img
                   saturation:@(0.76289f)
                   brightness:nil
@@ -192,7 +197,7 @@ static NSString* toBase64(NSData* data) {
                         blue:@(0.8995f)];
 }
 
-- (CIImage *)applyIntenseEffect:(CIImage *)img {
+- (UIImage *)applyIntenseEffect:(UIImage *)img {
     return [self applyFilter:img
                   saturation:@(1.618557f)
                   brightness:@(-0.1495f)
@@ -206,7 +211,7 @@ static NSString* toBase64(NSData* data) {
                         blue:@(0.8505f)];
 }
 
-- (CIImage *)applyWarmEffect:(CIImage *)img {
+- (UIImage *)applyWarmEffect:(UIImage *)img {
     return [self applyFilter:img
                   saturation:@(1.45876f)
                   brightness:nil
@@ -220,7 +225,7 @@ static NSString* toBase64(NSData* data) {
                         blue:nil];
 }
 
-- (CIImage *)applyLightEffect:(CIImage *)img {
+- (UIImage *)applyLightEffect:(UIImage *)img {
     return [self applyFilter:img
                   saturation:@(0.99484f)
                   brightness:@(-0.005155f)
@@ -234,7 +239,7 @@ static NSString* toBase64(NSData* data) {
                         blue:nil];
 }
 
-- (CIImage *)applyNoEffect:(CIImage *)img {
+- (UIImage *)applyNoEffect:(UIImage *)img {
     return [self applyFilter:img
                   saturation:nil
                   brightness:nil
@@ -248,71 +253,140 @@ static NSString* toBase64(NSData* data) {
                         blue:nil];
 }
 
-- (CIImage *)applyFilter:(CIImage*)image saturation:(NSNumber *)saturation brightness:(NSNumber*)brightness contrast:(NSNumber*)contrast gamma:(NSNumber*)gamma exposure:(NSNumber*)exposure sharpen:(NSNumber*)sharpen hue:(NSNumber*)hue red:(NSNumber*)red green:(NSNumber*)green blue:(NSNumber*)blue {
-    
-    CIImage *result = image;
-    
-    if (saturation)
-        result = [self applySaturation:result saturation:saturation];
-    if (brightness)
-        result = [self applyBrightness:result brightness:brightness];
-    if (contrast)
-        result = [self applyContrast:result contrast:contrast];
-    if (gamma)
-        result = [self applyGamma:result value:gamma];
-    if (exposure)
-        result = [self applyExposure:result value:exposure];
-    if (sharpen)
-        result = [self applySharpen:result value:sharpen];
-    if (hue)
-        result = [self applyHue:result value:hue];
-    if (red)
-        result = [self applyRed:result value:red];
-    if (green)
-        result = [self applyGreen:result value:green];
-    if (blue)
-        result = [self applyBlue:result value:blue];
-    
+- (UIImage *)applyFilter:(UIImage *)image saturation:(NSNumber *)saturation brightness:(NSNumber *)brightness contrast:(NSNumber *)contrast gamma:(NSNumber *)gamma exposure:(NSNumber *)exposure sharpen:(NSNumber *)sharpen hue:(NSNumber *)hue red:(NSNumber *)red green:(NSNumber *)green blue:(NSNumber *)blue {
+
+    UIImage *result;
+    NSMutableArray<GPUImageFilter*> *filters = [NSMutableArray new];
+    GPUImageFilter *tmp;
+    if (saturation) {
+        GPUImageFilter *filter = [self applySaturation:result saturation:saturation];
+        [filters addObject:filter];
+        tmp = filter;
+    }
+    if (brightness) {
+        GPUImageFilter *filter = [self applyBrightness:result brightness:brightness];
+        [filters addObject:filter];
+        [tmp addTarget:filter];
+        tmp = filter;
+    }
+    if (contrast) {
+        GPUImageFilter *filter = [self applyContrast:result contrast:contrast];
+        [filters addObject:filter];
+        [tmp addTarget:filter];
+        tmp = filter;
+    }
+    if (gamma){
+        GPUImageFilter *filter = [self applyGamma:result value:gamma];
+        [filters addObject:filter];
+        [tmp addTarget:filter];
+        tmp = filter;
+    }
+    if (exposure){
+        GPUImageFilter *filter = [self applyExposure:result value:exposure];
+        [filters addObject:filter];
+        [tmp addTarget:filter];
+        tmp = filter;
+    }
+    if (sharpen){
+        GPUImageFilter *filter = [self applySharpen:result value:sharpen];
+        [filters addObject:filter];
+        [tmp addTarget:filter];
+        tmp = filter;
+    }
+    if (hue){
+        GPUImageFilter *filter = [self applyHue:result value:hue];
+        [filters addObject:filter];
+        [tmp addTarget:filter];
+        tmp = filter;
+    }
+    if (red){
+        GPUImageFilter *filter = [self applyRed:result value:red];
+        [filters addObject:filter];
+        [tmp addTarget:filter];
+        tmp = filter;
+    }
+    if (green){
+        GPUImageFilter *filter = [self applyGreen:result value:green];
+        [filters addObject:filter];
+        [tmp addTarget:filter];
+        tmp = filter;
+    }
+    if (blue){
+        GPUImageFilter *filter = [self applyBlue:result value:blue];
+        [filters addObject:filter];
+        [tmp addTarget:filter];
+        tmp = filter;
+    }
+
+    GPUImagePicture *stillImageSource = [[GPUImagePicture alloc] initWithImage:image];
+    GPUImageFilterGroup *group = [GPUImageFilterGroup new];
+    [group setInitialFilters:[NSArray arrayWithObject:filters.firstObject]];
+    [group setTerminalFilter:filters.lastObject];
+
+    [stillImageSource addTarget:group];
+    [group useNextFrameForImageCapture];
+    [stillImageSource processImage];
+
+    result = [group imageFromCurrentFramebuffer];
+
     return result;
 }
 
-- (CIImage *)applyBrightness:(CIImage *)image brightness:(NSNumber*)brightness {
-    return [image imageByApplyingFilter:@"CIColorControls" withInputParameters:@{kCIInputBrightnessKey: brightness}];
+- (GPUImageFilter *)applyBrightness:(UIImage *)image brightness:(NSNumber*)brightness {
+    GPUImageBrightnessFilter *filter = [GPUImageBrightnessFilter new];
+    [filter setBrightness:[brightness floatValue]];
+    return filter;
+
 }
-- (CIImage *)applySaturation:(CIImage *)image saturation:(NSNumber*)saturation {
-    return [image imageByApplyingFilter:@"CIColorControls" withInputParameters:@{kCIInputSaturationKey: saturation}];
+- (GPUImageFilter *)applySaturation:(UIImage *)image saturation:(NSNumber*)saturation {
+    GPUImageSaturationFilter *filter = [GPUImageSaturationFilter new];
+    [filter setSaturation:[saturation floatValue]];
+    return filter;
 }
-- (CIImage *)applyContrast:(CIImage *)image contrast:(NSNumber*)contrast {
-    return [image imageByApplyingFilter:@"CIColorControls" withInputParameters:@{kCIInputContrastKey: contrast}];
+- (GPUImageFilter *)applyContrast:(UIImage *)image contrast:(NSNumber*)contrast {
+    GPUImageContrastFilter *filter = [GPUImageContrastFilter new];
+    [filter setContrast:[contrast floatValue]];
+    return filter;
 }
 
-- (CIImage *)applyExposure:(CIImage *)image value:(NSNumber*)value {
-    return [image imageByApplyingFilter:@"CIExposureAdjust" withInputParameters:@{kCIInputEVKey: value}];
+- (GPUImageFilter *)applyExposure:(UIImage *)image value:(NSNumber*)value {
+    GPUImageExposureFilter *filter = [GPUImageExposureFilter new];
+    [filter setExposure:[value floatValue]];
+    return filter;
 }
 
-- (CIImage *)applyGamma:(CIImage *)image value:(NSNumber*)value {
-    return [image imageByApplyingFilter:@"CIGammaAdjust" withInputParameters:@{@"inputPower": value}];
+- (GPUImageFilter *)applyGamma:(UIImage *)image value:(NSNumber*)value {
+    GPUImageGammaFilter *filter = [GPUImageGammaFilter new];
+    [filter setGamma:[value floatValue]];
+    return filter;
 }
 
-- (CIImage *)applySharpen:(CIImage *)image value:(NSNumber*)value {
-    return [image imageByApplyingFilter:@"CISharpenLuminance" withInputParameters:@{kCIInputSharpnessKey: value}];
+- (GPUImageFilter *)applySharpen:(UIImage *)image value:(NSNumber*)value {
+    GPUImageSharpenFilter *filter = [GPUImageSharpenFilter new];
+    [filter setSharpness:[value floatValue]];
+    return filter;
 }
 
-- (CIImage *)applyHue:(CIImage *)image value:(NSNumber*)value {
-    return [image imageByApplyingFilter:@"CIHueAdjust" withInputParameters:@{kCIInputAngleKey: value}];
+- (GPUImageFilter *)applyHue:(UIImage *)image value:(NSNumber*)value {
+    GPUImageHueFilter *filter = [GPUImageHueFilter new];
+    [filter setHue:[value floatValue]];
+    return filter;
 }
 
-- (CIImage*)applyRed:(CIImage*)image value:(NSNumber *)value {
-    CIVector *vector = [CIVector vectorWithX:[value floatValue]];
-    return [image imageByApplyingFilter:@"CIColorMatrix" withInputParameters:@{@"inputRVector": vector}];
+- (GPUImageFilter*)applyRed:(UIImage*)image value:(NSNumber *)value {
+    GPUImageRGBFilter *filter = [GPUImageRGBFilter new];
+    [filter setRed:[value floatValue]];
+    return filter;
 }
-- (CIImage*)applyGreen:(CIImage*)image value:(NSNumber *)value {
-    CIVector *vector = [CIVector vectorWithX:0 Y:[value floatValue]];
-    return [image imageByApplyingFilter:@"CIColorMatrix" withInputParameters:@{@"inputGVector": vector}];
+- (GPUImageFilter*)applyGreen:(UIImage*)image value:(NSNumber *)value {
+    GPUImageRGBFilter *filter = [GPUImageRGBFilter new];
+    [filter setGreen:[value floatValue]];
+    return filter;
 }
-- (CIImage*)applyBlue:(CIImage*)image value:(NSNumber *)value {
-    CIVector *vector = [CIVector vectorWithX:0 Y:0 Z:[value floatValue]];
-    return [image imageByApplyingFilter:@"CIColorMatrix" withInputParameters:@{@"inputBVector": vector}];
+- (GPUImageFilter*)applyBlue:(UIImage*)image value:(NSNumber *)value {
+    GPUImageRGBFilter *filter = [GPUImageRGBFilter new];
+    [filter setBlue:[value floatValue]];
+    return filter;
 }
 
 @end
