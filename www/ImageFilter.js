8dbcc7ed9e1519c51a32c300e0e9dce241cc3d21
ttdat89 <ttdat89@gmail.com>
2017-03-10 09:48:24 +0700

    fix bug

--- www/ImageFilter.js
+++ www/ImageFilter.js
@@ -1,9 +1,9 @@
 var exec = require('cordova/exec');
 
 exports.applyEffect = function(path, filterType, compressQuality, success, error) {
-    exec(success, error, "ImageFilter", "applyEffect", [path, filterType, compressQuality]);
+    exec(success, error, "ImageFilter", "applyEffect", [path, filterType, compressQuality, isBase64Image]);
 };
 
 exports.applyEffectForReview = function(path, filterType, compressQuality, success, error) {
-    exec(success, error, "ImageFilter", "applyEffectForReview", [path, filterType, compressQuality]);
+    exec(success, error, "ImageFilter", "applyEffectForReview", [path, filterType, compressQuality, isBase64Image]);
 };
