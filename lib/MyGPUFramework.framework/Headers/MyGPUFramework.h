24e6b6af31fb074a02c0063619fb89a58c60c3e7
ttdat89 <ttdat89@gmail.com>
2017-10-26 11:19:39 +0700

    add ios GPUImage framework

---
+++ lib/MyGPUFramework.framework/Headers/MyGPUFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyGPUFramework.h
+//  MyGPUFramework
+//
+//  Created by dat tran on 3/10/17.
+//  Copyright © 2017 sts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GPUImage.h"
+//! Project version number for MyGPUFramework.
+FOUNDATION_EXPORT double MyGPUFrameworkVersionNumber;
+
+//! Project version string for MyGPUFramework.
+FOUNDATION_EXPORT const unsigned char MyGPUFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyGPUFramework/PublicHeader.h>
+
+
